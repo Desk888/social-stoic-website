c73370ebb993ba3208e30146cc89ba0c01c6fdbc
Desk888 <work.lorenzofilippinI@gmail.com>
2025-03-28 09:53:52 +0000

    Added metadata

--- src/pages/Coaching.tsx
+++ src/pages/Coaching.tsx
@@ -23,7 +23,7 @@ const coachingServices = [
     id: 'online',
     title: '1-on-1 Coaching Online',
     description: 'Remote coaching sessions tailored to your specific needs, helping you develop the mindset and skills needed for successful dating.',
-    imageSrc: 'https://plus.unsplash.com/premium_photo-1661766473438-0509d5f19ab8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageSrc: 'https://images.unsplash.com/photo-1474403078171-7f199e9d1335?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     benefits: [
       'Flexible scheduling from anywhere in the world',
       'Personalised curriculum based on your dating challenges',
